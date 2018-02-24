8b0a5bdab9029785bc50968a46a5de9760dc37e2
Pablo Hinojosa <pablohn6@gmail.com>
2018-02-24 01:10:20 +0100

    Add My Youtube API Key

--- app/Environment.js
+++ app/Environment.js
@@ -1,6 +1,6 @@
 var Environment = {
-  'youtubeApiKey': 'AIzaSyC4W2PcBHccKu03OiW8l5Ff8LfsgdS5C44',
-  // 'youtubeApiKey': '<YOUR_API_KEY>',
+//  'youtubeApiKey': 'AIzaSyC4W2PcBHccKu03OiW8l5Ff8LfsgdS5C44',
+  'youtubeApiKey': 'AIzaSyDd7zlncvb1gxMJrFm5U-wysJiNSVGa9NA	',
   'maxResults': 50, // [0, 50]
   'creativeCommon' : true
 };
